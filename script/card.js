95290164d8ddff9410746fbb2ebc48167d31c3c0
¨felipe¨ <juanpipo03@gmail.com>
2024-09-12 17:31:02 -0500

    ultimo arreglo

---
+++ script/card.js
@@ -0,0 +1,14 @@
+/*class card {
+  constructor(name, link) {
+    (this.name = name), (this.link = link);
+  }
+  getTemplate() {
+    return cardTemplate.queryselector(".card").cloneNode(true);
+  }
+  setProperties() {}
+}
+
+const card = {
+  name: "",
+  link: "",
+};
